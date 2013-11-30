1780f484aaea25e58c69763e7c8c079c3df8acd6
Christopher Jones <christopherdavidjones@gmail.com>
2013-11-30 06:23:14 -0500

    Grab the first available activity stream, geojsonize, send to geojson.io Clean this up!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
@@ -7,6 +8,10 @@ var app = express();
 var settings = JSON.parse(fs.readFileSync('stravaSettings.json', 'utf-8'));
 
 app.use(express.static('assets'));
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'joanie' }));
+
+var token;
 
 // Prompt user to authenticate with Strava.
 app.get('/', function(req, res) {
@@ -25,7 +30,8 @@ app.get('/token-exchange', function(req, res) {
     }, function(err, resp) {
         if (err) res.send(500);
 
-        var token = JSON.parse(resp.body).access_token;
+        token = JSON.parse(resp.body).access_token;
+        req.session.token = token;
 
         // For now, just spit back activities.
         request.get({
@@ -35,11 +41,57 @@ app.get('/token-exchange', function(req, res) {
             }
         }, function(err, resp) {
             if (err) res.send(500);
+
             var activities = JSON.parse(resp.body);
-            res.json(activities);
+
+            // Just grab whatever the first activity is for now
+            getActivityStream(_(activities).first().id, function(err, act) {
+                if (err) res.send(500);
+                // Send it over to geojson.io
+                res.redirect("http://geojson.io/#data=data:application/json," + encodeURIComponent(JSON.stringify(act));
+            });
         });
     });
 });
+
+// Get an activity "stream"
+function getActivityStream(id, callback) {
+    request.get({
+        url: 'https://www.strava.com/api/v3/activities/' + id + '/streams/latlng',
+        headers: {
+            'Authorization': 'access_token ' + token
+        }
+    }, function(err, resp) {
+        if (err) return callback(err);
+
+        // parse the activity
+        // @TODO error check!
+        var stream = JSON.parse(resp.body);
+
+        // Force geojson structure.
+        var geojson = {};
+        geojson.type = "FeatureCollection";
+        geojson.features = [];
+
+        var feat = {};
+        feat.type = "Feature";
+        feat.properties = {};
+        feat.geometry = {};
+        feat.geometry.type = "MultiLineString";
+        feat.geometry.coordinates = [];
+        feat.geometry.coordinates[0] = [];
+
+        // add arays of points
+        for (var pt in stream[0].data) {
+            feat.geometry.coordinates[0].push(stream[0].data[pt].reverse());    //Strava points are in reverse order
+        }
+
+        // Shove in the feature
+        geojson.features.push(feat);
+
+        callback(null, geojson);
+    });
+}
 
 app.listen(3000);
 console.log('Listening on port 3000');
